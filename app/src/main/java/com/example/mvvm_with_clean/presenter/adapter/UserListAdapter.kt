5db072bd2b4926f8bb55b046cf150b3725bd159a
EndrylCristian <endrylfiorotti.accenture@natura.net>
2023-07-07 13:32:01 -0300

    :sparkles: Adding bottom sheet to present user information

--- app/src/main/java/com/example/mvvm_with_clean/presenter/adapter/UserListAdapter.kt
+++ app/src/main/java/com/example/mvvm_with_clean/presenter/adapter/UserListAdapter.kt
@@ -8,7 +8,8 @@ import com.example.mvvm_with_clean.databinding.ItemListBinding
 import com.example.mvvm_with_clean.domain.presentation.UserPresentation
 
 class UserListAdapter(
-    private val userList: List<UserPresentation>
+    private val userList: List<UserPresentation>,
+    private val onClick: (UserPresentation) -> Unit
 ) : RecyclerView.Adapter<UserListAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -22,15 +23,18 @@ class UserListAdapter(
         holder.bind()
     }
 
-    inner class ViewHolder(private val binding: ItemListBinding) :
-        RecyclerView.ViewHolder(binding.root) {
+    inner class ViewHolder(private val binding: ItemListBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            binding.root.setOnClickListener { onClick(userData()) }
+        }
+
         fun bind() {
             with(binding) {
                 tvName.text = userData().name
                 tvAge.text = tvAge.context.getString(R.string.presentation_age, userData().age)
                 tvEmail.text = userData().email
             }
-
         }
 
         private fun userData() = userList[adapterPosition]
--- app/src/main/java/com/example/mvvm_with_clean/presenter/view/fragment/UserListFragment.kt
+++ app/src/main/java/com/example/mvvm_with_clean/presenter/view/fragment/UserListFragment.kt
@@ -7,9 +7,12 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import coil.load
 import com.example.mvvm_with_clean.databinding.FragmentUserListBinding
+import com.example.mvvm_with_clean.domain.presentation.UserPresentation
 import com.example.mvvm_with_clean.presenter.adapter.UserListAdapter
 import com.example.mvvm_with_clean.presenter.viewModel.UserViewModel
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class UserListFragment : Fragment() {
@@ -28,6 +31,15 @@ class UserListFragment : Fragment() {
 
         setUpObservers()
         setUpRequestApp()
+        bottomSheetConfig()
+    }
+
+    private fun bottomSheetConfig() {
+        binding.flBottomSheet.isVisible = false
+        BottomSheetBehavior.from(binding.flBottomSheet).apply {
+            peekHeight = 100
+            this.state = BottomSheetBehavior.STATE_COLLAPSED
+        }
     }
 
     private fun setUpRequestApp() {
@@ -38,10 +50,8 @@ class UserListFragment : Fragment() {
 
     private fun setUpObservers() {
         with(viewModel) {
-            userList.observe(viewLifecycleOwner) {
-                binding.rvUsers.adapter = UserListAdapter(it)
-                binding.swipeToRefresh.isRefreshing = false
-                stopLoading()
+            userList.observe(viewLifecycleOwner) { list ->
+                recyclerViewConfig(list)
             }
             isEmpty.observe(viewLifecycleOwner) {
                 Toast.makeText(requireContext(), "List is empty.", Toast.LENGTH_SHORT).show()
@@ -49,6 +59,23 @@ class UserListFragment : Fragment() {
             error.observe(viewLifecycleOwner) {
                 Toast.makeText(requireContext(), "There was an error. ${it.message}", Toast.LENGTH_SHORT).show()
             }
+        }
+    }
+
+    private fun recyclerViewConfig(list: List<UserPresentation>) {
+        binding.rvUsers.adapter = UserListAdapter(list) {
+            bottomSheetLayout(it)
+        }
+        binding.swipeToRefresh.isRefreshing = false
+        stopLoading()
+    }
+
+    private fun bottomSheetLayout(it: UserPresentation) {
+        with(binding) {
+            flBottomSheet.isVisible = true
+            ivUserImage.load(it.image)
+            tvUserName.text = it.name
+            tvUserEmail.text = it.email
         }
     }
 
